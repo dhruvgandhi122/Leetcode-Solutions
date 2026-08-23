5140c403f90156449b5c284d3dc4d3bc69cc3ebc
Dhruv Gandhi <dhruvgandhi122@gmail.com>
2026-08-23 16:40:21 +0530

    Time: 0 ms (100%), Space: 46.5 MB (63.27%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,69 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int peak = peakIndexInMountainArray(mountainArr);
+        if (peak == -1) {
+            return -1;
+        }
+    
+        int firstTry = OrderAgnosticBinarySearch(mountainArr, target, 0, peak);
+        if (firstTry != -1) {
+            return firstTry;
+        }
+        
+        int secondTry = OrderAgnosticBinarySearch(mountainArr, target, peak + 1, mountainArr.length() - 1);
+        if (secondTry != -1) {
+            return secondTry;
+        }
+        return -1;
+    }
+
+    public int peakIndexInMountainArray(MountainArray arr) {
+        int beg = 0;
+        int end = arr.length() - 1;
+        
+        while (beg < end) {
+            int mid = beg + (end - beg) / 2;
+            if (arr.get(mid) > arr.get(mid + 1)) {
+                end = mid; // Mid could be the peak, so don't subtract 1
+            } else {
+                beg = mid + 1;
+            }
+        }
+        return beg;
+    }
+
+    public int OrderAgnosticBinarySearch(MountainArray arr, int target, int start, int end) {
+        boolean isAsc = arr.get(start) < arr.get(end);
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int midVal = arr.get(mid);
+
+            if (midVal == target) {
+                return mid;
+            }
+            if (isAsc) {
+                if (target < midVal) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target < midVal) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
