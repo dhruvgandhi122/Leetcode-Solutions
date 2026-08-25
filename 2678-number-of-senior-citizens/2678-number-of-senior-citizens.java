2368e102bcddab90006da205a37125d9469fbe92
Dhruv Gandhi <dhruvgandhi122@gmail.com>
2026-08-25 12:54:11 +0530

    Time: 1 ms (69.16%), Space: 44.2 MB (58.41%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -2,21 +2,19 @@ class Solution {
     public int countSeniors(String[] details) {
         int count=0;
         for(String s:details){
-            if(60<((Character.getNumericValue(s.charAt(11)))*10)+(Character.getNumericValue(s.charAt(12)))){
-                count+=1;
+            if(seniorCitizen(s)){
+                count++;
             }
         }
         return count; 
     }
-}
-    /*public boolean seniorCitizen(String s){
+    public boolean seniorCitizen(String s){
         int num1=Character.getNumericValue(s.charAt(11));
         int num2=Character.getNumericValue(s.charAt(12));
         int age =(num1*10)+num2;
-        if(age>=60){
+        if(age>60){
             return true;
         }
         return false;
     }
 }
-*/
